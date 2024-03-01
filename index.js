63b7ced653e500e69c6a9df9d6d89502815c4605
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-01 17:13:33 +0300

    complete  Assigmmnet

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(records) {
+  let data = records.find((record) => record.result === "W");
+  return data ? data.year : undefined;
+}
+
